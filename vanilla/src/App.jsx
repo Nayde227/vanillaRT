0d6ff7f2703f4a721a280f8bec35476394081623
Nayde227 <naylimar27@gmail.com>
2024-06-15 22:54:54 -0400

    add/ cards w/colors and pos

--- vanilla/src/App.jsx
+++ vanilla/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import coheteImage from './assets/cohete.png';
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-    <header>Texto para el header</header>
+      <header>Texto para el header</header>
       <nav>
-        <div className='logo'><h1>LOGO</h1></div>
+        <div className='logo'><h2>LOGO</h2></div>
         <div className='navBar'>
           <a href="">Inicio</a>
           <a>Sección 2</a>
@@ -16,12 +17,32 @@ function App() {
         </div>
       </nav>
       <section className='banner'>
-        
+
         <div className="overlay"></div>
         <div className="banner-content">
           <h3>Texto previo al título</h3>
           <h1>Título banner principal</h1>
           <button className='enviar'>Enviar</button>
+        </div>
+      </section>
+      <section className='boxes'>
+        <div className='box1'>
+          <img className='cohete' src={coheteImage}></img>
+          <h2 className='Item'>Item 1</h2>
+          <p>Aún nuestro sitio está en cosntrucción (somos muy perfeccionistas jeje).
+          Mientras puedes navegar en nuestro sitio antiguo.</p>
+        </div>
+        <div className='box1'>
+          <img className='cohete' src={coheteImage}></img>
+          <h2 className='Item'>Item 1</h2>
+          <p className='text'>Aún nuestro sitio está en cosntrucción (somos muy perfeccionistas jeje).
+          Mientras puedes navegar en nuestro sitio antiguo.</p>
+        </div>
+        <div className='box1'>
+          <img className='cohete' src={coheteImage}></img>
+          <h2 className='Item'>Item 1</h2>
+          <p>Aún nuestro sitio está en cosntrucción (somos muy perfeccionistas jeje).
+          Mientras puedes navegar en nuestro sitio antiguo.</p>
         </div>
       </section>
     </>
